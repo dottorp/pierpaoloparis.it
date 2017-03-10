5d90240a1a3e75c3df250629170e06809a633a1b
Pierpaolo Paris <pparis@progesoftware.it>
2017-03-10 06:21:58 +0100

    ThreadLocal, ThreadStatic

---
+++ Thread/Thread/ThreadLocal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace EsempiThread
+{
+    public class ThreadLocal : IEsempio
+    {
+        private ThreadLocal<int> interoRandom;
+        public ThreadLocal()
+        {
+            Random randomGenerator = new Random(DateTime.Now.Second);
+            interoRandom = new ThreadLocal<int>(() => randomGenerator.Next(15));
+        }
+        public void Run()
+        {
+            Thread t1 = new Thread(() =>
+            {
+                for (int x = 0; x < interoRandom.Value; x++)
+                {
+                    Console.WriteLine("Thread A: {0}", x);
+                }
+            });
+            Thread t2 = new Thread(() =>
+            {
+                for (int x = 0; x < interoRandom.Value; x++)
+                {
+                    Console.WriteLine("Thread B {0}", x);
+                }
+            });
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();            
+            Console.WriteLine("Esecuzione terminata, premere un tasto per terminare.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Thread/Thread/ThreadStatic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+namespace EsempiThread
+{
+    public class ThreadStatic : IEsempio
+    {
+        public static int contatore;
+        public void Run()
+        {
+            new Thread(() =>
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    contatore++;
+                    Console.WriteLine("Thread A: {0}", contatore);
+                }
+            }).Start();
+            new Thread(() =>
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                contatore++;
+                Console.WriteLine("Thread B: {0}", contatore);
+            }
+        }).Start();
+            Console.ReadKey();
+        }
+    }
+}
